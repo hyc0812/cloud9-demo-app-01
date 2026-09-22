055f09e7ca19a3e66bd4c73cc24eba2e93e20568
EC2 Default User <ec2-user@ip-172-31-24-133.ec2.internal>
2026-09-22 21:24:14 +0000

    modify name

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
         transition-transform duration-300 hover:scale-105'/>
         <h1 className='text-4xl font-bold'>
             I'm {" "}
-            <span className='text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500'>Mohammad Yousof</span>
+            <span className='text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500'>Yongchang He</span>
             , Full-Stack Developer
         </h1>
         <p className='mt-4 text-lg text-gray-300'>
